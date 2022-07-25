5b6fab76fa071f7d5eaff808df2b6d8ae4431154
Nikita Kochetov <nothsmple@mail.ru>
2022-07-25 15:55:58 +0300

    geocoder placemark performingReverseGeocoding lastGeocodingError have been added.

--- MyBestLocations/CurrentLocationViewController.swift
+++ MyBestLocations/CurrentLocationViewController.swift
@@ -22,6 +22,12 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     var updatingLocation = false
     var lastLocationError: Error?
     
+    let geocoder = CLGeocoder()
+    var placemark: CLPlacemark?
+    var performingReverseGeocoding = false
+    var lastGeocodingError: Error?
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabels()
