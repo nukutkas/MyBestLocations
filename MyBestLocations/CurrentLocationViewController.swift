31aa66ce00106989c479f9b57535fd7f63ec7eee
Nikita Kochetov <nothsmple@mail.ru>
2022-08-15 14:54:08 +0300

    isNavigationBarHidden has been added to viewWillAppear and viewWillDisappear

--- MyBestLocations/CurrentLocationViewController.swift
+++ MyBestLocations/CurrentLocationViewController.swift
@@ -35,6 +35,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         updateLabels()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     // MARK: - Actions
     @IBAction func getLocation() {
         let authStatus = locationManager.authorizationStatus
