1fe876679d35732beb6b2fb180247ccc3585e897
Nikita Kochetov <65461320+nukutkas@users.noreply.github.com>
2022-09-02 10:54:37 +0300

    hideKeyboard has been added.

--- MyBestLocations/LocationDetailsViewController.swift
+++ MyBestLocations/LocationDetailsViewController.swift
@@ -48,6 +48,14 @@ class LocationDetailsViewController: UITableViewController {
         }
         
         dateLabel.text = format(date: Date())
+        
+        // Hide keyboard
+        let gestureRecognizer = UITapGestureRecognizer(
+          target: self,
+          action: #selector(hideKeyboard))
+        gestureRecognizer.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(gestureRecognizer)
+
     }
     
     
@@ -97,6 +105,19 @@ class LocationDetailsViewController: UITableViewController {
     func format(date: Date) -> String {
         return dateFormatter.string(from: date)
     }
+    
+    @objc func hideKeyboard(
+      _ gestureRecognizer: UIGestureRecognizer
+    ) {
+      let point = gestureRecognizer.location(in: tableView)
+      let indexPath = tableView.indexPathForRow(at: point)
+
+      if indexPath != nil && indexPath!.section == 0 &&
+          indexPath!.row == 0 {
+        return
+      }
+      descriptionTextView.resignFirstResponder()
+    }
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "PickCategory" {
@@ -104,5 +125,27 @@ class LocationDetailsViewController: UITableViewController {
             controller.selectedCategoryName = categoryName
         }
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+      if indexPath.section == 0 || indexPath.section == 1 {
+        return indexPath
+      } else {
+        return nil
+      }
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      didSelectRowAt indexPath: IndexPath
+    ) {
+      if indexPath.section == 0 && indexPath.row == 0 {
+        descriptionTextView.becomeFirstResponder()
+      }
+    }
+
 }
 
