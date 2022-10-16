f84460996146be0371d90cfcd7890aef5e79c772
Nikita Kochetov <65461320+nukutkas@users.noreply.github.com>
2022-10-16 16:25:53 +0300

    Global applicationDocumentsDirectory has been added.

--- MyBestLocations/Functions.swift
+++ MyBestLocations/Functions.swift
@@ -13,3 +13,10 @@ func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
     execute: run)
 }
 
+let applicationDocumentsDirectory: URL = {
+  let paths = FileManager.default.urls(
+    for: .documentDirectory,
+    in: .userDomainMask)
+  return paths[0]
+}()
+
